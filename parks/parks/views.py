1c86df5bcdfc8d6a06e290fa98b850e46bff227f
henrykh <henryhowes@gmail.com>
2015-04-23 13:27:11 -0700

    adds basic mapbox map

--- parks/parks/views.py
+++ parks/parks/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from secrets import *
 
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'mapbox_access_token': MAPBOX_ACCESS_TOKEN}
+    return render(request, 'home.html', context)
