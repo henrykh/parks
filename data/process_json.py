0693adfc6dfffa96adfe7706cc09d1245f84bb5f
henrykh <henryhowes@gmail.com>
2015-04-24 16:06:50 -0700

    work on json to geojson processing script

--- data/process_json.py
+++ data/process_json.py
@@ -2,5 +2,22 @@ import json
 
 if __name__ == "__main__":
     with open("parks.json") as j:
-        parks = json.loads(j.read())
-        
+        parks = json.loads(j.read())['data']
+        parks_geojson = {"type": "FeatureCollection",
+                         "features": []}
+        for park in parks:
+            park_geojson = {"type": "Feature",
+                            "geometry": {"type": "Point",
+                                         "coordinates": "{}, {}".format(
+                                            park[12], park[13])},
+                            "properties": {
+                                "name": park[9],
+                                "address": park[10],
+                                "type": park[8],
+                                "website": park[11][0]
+                            }
+                            }
+            parks_geojson["features"].append(park_geojson)
+
+        with open('parks_geojson.json', 'w') as outfile:
+            json.dump(parks_geojson, outfile)
