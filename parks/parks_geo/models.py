482bd3ed04d87e964587d922dd51fcc28db99d87
henrykh <henryhowes@gmail.com>
2015-04-25 20:31:19 -0700

    work on park model

--- parks/parks_geo/models.py
+++ parks/parks_geo/models.py
@@ -6,7 +6,14 @@ from django.db import models
 # Think about relationship between parks and users, users and comments/reviews 
 
 
+# Read parks geojson and pass into database
+def initialize_parks():
+    pass
+
+
 class Park(models.Model):
     name = models.CharField(max_length=64)
     feature = models.CharField(max_length=64)
     address = models.CharField(max_length=64)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
